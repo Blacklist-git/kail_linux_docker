decb8adc850fa970a63feaccc4c8685a522ad4f5
KimWang906 <hyunbin06git@gmail.com>
2023-03-27 07:15:34 +0900

    Update: shell script, readme

--- init.sh
+++ init.sh
@@ -4,13 +4,23 @@ if [ "$(sudo docker ps -aq -f name=kail)" ]; then
     sudo docker rm -f kail
 fi
 
-cd config
-for file in *.ovpn; do
-    if [ "$file" != "groot_security_config.ovpn" ]; then
-        mv "$file" "groot_security_config.ovpn"
-    fi
-done
-cd ..
+if [ -d "config" ] && [ -f "config/groot_security_config.ovpn" ]; then
+  echo "config 디렉터리와 groot_security_config.ovpn 파일이 이미 존재합니다."
+else
+  if [ ! -d "config" ]; then
+    mkdir config
+  fi
+
+  mv *.ovpn config/
+
+  cd config
+  for file in *.ovpn; do
+      if [ "$file" != "groot_security_config.ovpn" ]; then
+          mv "$file" "groot_security_config.ovpn"
+      fi
+  done
+  cd ..
+fi
 
 sudo docker build --network host -t kail_linux:0.0 `pwd`
 sudo docker run --name kail --privileged --security-opt seccomp=unconfined --net host -i -t kail_linux:0.0
--- readme.md
+++ readme.md
@@ -5,5 +5,4 @@
 1. [tryhackme](https://tryhackme.com/)로 접속하여 회원가입
 2. [Groot Security Room](https://tryhackme.com/room/grootsecurity)에 접속
 3. [OpenVPN](https://tryhackme.com/access) 설정 파일 다운로드(서버는 **AU-Regular-1**를 추천)
-4. 다운로드 받은 파일을 해당 리포지토리의 `config`를 만들어 넣는다.
-5. `chmod +x ./init.sh`를 한 후 `./init.sh`를 실행한다.
+4. `chmod +x ./init.sh`를 한 후 `./init.sh`를 실행한다.
